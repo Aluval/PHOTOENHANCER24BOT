3d6f6b5b34ffc1350d4c025bb16cb17054ef08d2
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-08 19:06:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -262,6 +262,37 @@ def resize_photo(image_path):
     image = Image.open(image_path)
     resized_image = ImageOps.fit(image, (300, 300))  # Adjust the size as needed
     return resized_image
-        
+    
+ API = "https://apis.xditya.me/lyrics?song="
+
+@app.on_message(filters.command(["lyrics"]))
+async def sng(bot, message):
+        if not message.reply_to_message:
+          await message.reply_text("Please reply to a message")
+        else:          
+          mee = await message.reply_text("`Searching 🔎`")
+          song = message.reply_to_message.text
+          chat_id = message.from_user.id
+          rpl = lyrics(song)
+          await mee.delete()
+          try:
+            await mee.delete()
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/Sunrises24BotUpdates")]]))
+          except Exception as e:                            
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/Sunrises24BotUpdates")]]))
+
+
+def search(song):
+        r = requests.get(API + song)
+        find = r.json()
+        return find
+       
+def lyrics(song):
+        fin = search(song)
+        text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
+        text += f'`{fin["lyrics"]}`'
+        text += '\n\n\n**Made By Sᴜɴʀɪsᴇs Hᴀʀsʜᴀ 𝟸𝟺 🇮🇳 ᵀᴱᴸ**'
+        return text
+    
 # Run the bot
 app.run()
